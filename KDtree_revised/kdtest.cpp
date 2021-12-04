f6b53593abfacc5b0b034eca3f59621b5b8c3d78
JunboShen <93768018+JunboShen@users.noreply.github.com>
2021-12-04 16:50:31 +0800

    Add files via upload

--- KDtree_revised/kdtest.cpp
+++ KDtree_revised/kdtest.cpp
@@ -4,13 +4,13 @@
 #include <math.h>
 #include <memory>
 using namespace std;
-const int k=2;
+const int k = 2;
 #ifndef KDTREE_H
 #include "kdtree.h"
 #endif
 int main()
-{   
-    vectordata<double,int>set[100]; //build empty set
+{
+    vectordata<double, int> set[100]; //build empty set
     //测试分到另一个文件，clangformat，*template*，内存，智能指针（sharedptr，uniqueptr）基于模板
     //socket programming通信库，Epoll，kubenetes
 
@@ -34,26 +34,31 @@ int main()
 
     //cout<<id;
     //struct Tnode<double,int> *root = NULL;
-    shared_ptr<Tnode<double,int>> root(new Tnode<double,int>) ;
-    root = build_kdtree<double,int>(set, id, root);
+    shared_ptr<Tnode<double, int>> root(new Tnode<double, int>);
+    root = build_kdtree<double, int>(set, id, root);
 
     double point1[] = {0, 6}; //a new set
 
-    insertnew<double,int>(root, point1, set, id);//insert new data
+    insertnew<double, int>(root, point1, set, id); //insert new data
 
     //test
-    cout << set[7].number[0] << set[7].number[1] << "\n";
+    cout << set[0].number[0] << set[0].number[1] << "\n";
+    cout << set[1].number[0] << set[1].number[1] << "\n";
+    cout << set[2].number[0] << set[2].number[1] << "\n";
+    cout << set[3].number[0] << set[3].number[1] << "\n";
+    cout << set[4].number[0] << set[4].number[1] << "\n";
+    cout << set[5].number[0] << set[5].number[1] << "\n";
     cout << set[6].number[0] << set[6].number[1] << "\n";
-    (search<double,int>(root, point1)) ? cout << "Found\n" : cout << "Not Found\n";
+    (search<double, int>(root, point1)) ? cout << "Found\n" : cout << "Not Found\n";
 
-    root = deleteNode<double,int>(root, point1);
-    id = deletedata<double,int>(set, point1, id); //delete
-    
+    root = deleteNode<double, int>(root, point1);
+    id = deletedata<double, int>(set, point1, id); //delete
+
     root = NULL;
-    root=build_kdtree(set, id, root);//update
+    root = build_kdtree(set, id, root); //update
 
     //test
-    (search<double,int>(root, point1)) ? cout << "Found\n" : cout << "Not Found\n";
+    (search<double, int>(root, point1)) ? cout << "Found\n" : cout << "Not Found\n";
     cout << root->left->left->da.number[0] << root->left->left->da.number[1] << "\n";
     cout << root->split << "\n";
     cout << id;
--- KDtree_revised/kdtree.h
+++ KDtree_revised/kdtree.h
@@ -8,33 +8,32 @@ using namespace std;
 #define KDTREE_H
 
 //const int k=i;//写成变量
-template <class T,class T1>
-struct vectordata //太常见
+template <class T, class T1>
+struct vectordata 
 {
     T number[k];
 };
-template <class T,class T1>
+template <class T, class T1>
 struct Tnode
-{   
-    struct vectordata<T,T1> da;
-    T1 split;
-    struct shared_ptr<Tnode<T,T1>> left;
-    struct shared_ptr<Tnode<T,T1>> right;
-};
-template <class T,class T1>
-struct shared_ptr<Tnode<T,T1>> newnode(T arr[])
 {
-    struct shared_ptr<Tnode<T,T1>> temp (new Tnode<double,int>);
+    struct vectordata<T, T1> da;
+    T1 split;
+    struct shared_ptr<Tnode<T, T1>> left;
+    struct shared_ptr<Tnode<T, T1>> right;
+};
+template <class T, class T1>
+struct shared_ptr<Tnode<T, T1>> newnode(T arr[])
+{
+    struct shared_ptr<Tnode<T, T1>> temp(new Tnode<double, int>);
     for (int i = 0; i < k; i++)
         temp->da.number[i] = arr[i];
     temp->left = temp->right = NULL;
     temp->split = 0;
     return temp;
-}
-template <class T,class T1>
-void sorting(vectordata<T,T1> set[], T1 size, T1 &split)
+} template <class T, class T1>
+void sorting(vectordata<T, T1> set[], T1 size, T1 &split)
 {
-    vectordata<T,T1> tmp;
+    vectordata<T, T1> tmp;
     for (int i = 0; i < size - 1; i++)
     {
         for (int j = size - 1; j > i; j--)
@@ -49,8 +48,8 @@ void sorting(vectordata<T,T1> set[], T1 size, T1 &split)
     }
 
 } //sort the vectordata set in an increasing order, consider the given dimension
-template <class T,class T1>
-bool equal(vectordata<T,T1> &a, vectordata<T,T1> &b) //常量引用，不用value
+template <class T, class T1>
+bool equal(vectordata<T, T1> &a, vectordata<T, T1> &b) //常量引用，不用value
 {
     for (int i = 0; i < k; i++)
     {
@@ -61,8 +60,8 @@ bool equal(vectordata<T,T1> &a, vectordata<T,T1> &b) //常量引用，不用valu
     }
     return true;
 }
-template <class T,class T1>
-void ChooseSplit(vectordata<T,T1> set[], T1 size, T1 &split, vectordata<T,T1> &SplitChoice)
+template <class T, class T1>
+void ChooseSplit(vectordata<T, T1> set[], T1 size, T1 &split, vectordata<T, T1> &SplitChoice)
 {
 
     double tmp1, tmp2;
@@ -85,8 +84,8 @@ void ChooseSplit(vectordata<T,T1> set[], T1 size, T1 &split, vectordata<T,T1> &S
             v2 = v1;
         }
 
-    }                          //choose dimension with the largest variance
-    sorting<T,T1>(set, size, split); //不要自己写排序
+    }                                 //choose dimension with the largest variance
+    sorting<T, T1>(set, size, split); //不要自己写排序
 
     //set the split point value
     for (int l = 0; l < k; l++)
@@ -94,8 +93,8 @@ void ChooseSplit(vectordata<T,T1> set[], T1 size, T1 &split, vectordata<T,T1> &S
         SplitChoice.number[l] = set[size / 2].number[l];
     }
 }
-template <class T,class T1>
-shared_ptr<Tnode<T,T1>> build_kdtree(vectordata<T,T1> set[], T1 size, shared_ptr<Tnode<T,T1>> Tn) //c++,模板参数template
+template <class T, class T1>
+shared_ptr<Tnode<T, T1>> build_kdtree(vectordata<T, T1> set[], T1 size, shared_ptr<Tnode<T, T1>> Tn) //c++,模板参数template
 {
     //call ChooseSplit to choose the dimension and split point
     if (size == 0)
@@ -105,10 +104,10 @@ shared_ptr<Tnode<T,T1>> build_kdtree(vectordata<T,T1> set[], T1 size, shared_ptr
     else
     {
         int split;
-        vectordata<T,T1> da;
+        vectordata<T, T1> da;
         ChooseSplit(set, size, split, da);
-        vectordata<T,T1> set_right[100];
-        vectordata<T,T1> set_left[100];
+        vectordata<T, T1> set_right[100];
+        vectordata<T, T1> set_left[100];
         int sizeleft, sizeright;
         sizeleft = sizeright = 0;
         for (int i = 0; i < size; ++i)
@@ -132,7 +131,7 @@ shared_ptr<Tnode<T,T1>> build_kdtree(vectordata<T,T1> set[], T1 size, shared_ptr
             }
         }
 
-        Tn.reset(new Tnode<T,T1>);
+        Tn.reset(new Tnode<T, T1>);
         for (int m = 0; m < k; m++)
         {
             Tn->da.number[m] = da.number[m];
@@ -146,7 +145,7 @@ shared_ptr<Tnode<T,T1>> build_kdtree(vectordata<T,T1> set[], T1 size, shared_ptr
 //build KD tree
 /////////////////////////////////////////////////////////////////////////////////////////////
 //Search
-template <class T,class T1>
+template <class T, class T1>
 bool arePointsSame(T point1[], T point2[])
 {
     for (int i = 0; i < k; ++i)
@@ -155,12 +154,12 @@ bool arePointsSame(T point1[], T point2[])
 
     return true;
 }
-template <class T,class T1>
-bool searchRec(shared_ptr<Tnode<T,T1>> root, T point[])
+template <class T, class T1>
+bool searchRec(shared_ptr<Tnode<T, T1>> root, T point[])
 {
     if (root == NULL)
         return false;
-    if (arePointsSame<T,T1>(root->da.number, point))
+    if (arePointsSame<T, T1>(root->da.number, point))
         return true;
     int cd = root->split;
 
@@ -169,20 +168,20 @@ bool searchRec(shared_ptr<Tnode<T,T1>> root, T point[])
 
     return searchRec(root->right, point);
 }
-template <class T,class T1>
-bool search(shared_ptr<Tnode<double,int>> root, T point[])
+template <class T, class T1>
+bool search(shared_ptr<Tnode<double, int>> root, T point[])
 {
-    return searchRec<T,T1>(root, point);
+    return searchRec<T, T1>(root, point);
 }
 //Search
 /////////////////////////////////////////////////////////////////////////////////////////
 //insert
-template <class T,class T1>
-shared_ptr<Tnode<T,T1>> insertRec(shared_ptr<Tnode<T,T1>> root, T point[])
+template <class T, class T1>
+shared_ptr<Tnode<T, T1>> insertRec(shared_ptr<Tnode<T, T1>> root, T point[])
 {
     if (root == NULL)
     {
-        return newnode<T,T1>(point);
+        return newnode<T, T1>(point);
     }
 
     int cd = root->split;
@@ -194,35 +193,35 @@ shared_ptr<Tnode<T,T1>> insertRec(shared_ptr<Tnode<T,T1>> root, T point[])
     return root;
 }
 template <class T, class T1>
-shared_ptr<Tnode<T,T1>> insert(shared_ptr<Tnode<T,T1>> root, T point[], vectordata<T,T1> set[], T1 id)
+shared_ptr<Tnode<T, T1>> insert(shared_ptr<Tnode<T, T1>> root, T point[], vectordata<T, T1> set[], T1 id)
 {
     for (int i = 0; i < k; i++)
     {
         set[id].number[i] = point[i];
     }
-    return insertRec<T,T1>(root, point);
+    return insertRec<T, T1>(root, point);
 }
 template <class T, class T1>
-int insertnew(shared_ptr<Tnode<double,int>> root, T point[], vectordata<T,T1> set[], T1 id)
+int insertnew(shared_ptr<Tnode<double, int>> root, T point[], vectordata<T, T1> set[], T1 id)
 {
-    insert<T,T1>(root, point, set, id);
+    insert<T, T1>(root, point, set, id);
     return id + 1;
 }
 //insert
 //////////////////////////////////////////////////////////////////////////////////////
 //delete
-template <class T,class T1>
-shared_ptr<Tnode<double,int>> minNode(shared_ptr<Tnode<double,int>> t1, shared_ptr<Tnode<double,int>> t2, shared_ptr<Tnode<double,int>> t3, T1 d)
+template <class T, class T1>
+shared_ptr<Tnode<double, int>> minNode(shared_ptr<Tnode<double, int>> t1, shared_ptr<Tnode<double, int>> t2, shared_ptr<Tnode<double, int>> t3, T1 d)
 {
-    shared_ptr<Tnode<double,int>> tmp = t1;
+    shared_ptr<Tnode<double, int>> tmp = t1;
     if (t2 != NULL && t2->da.number[d] < tmp->da.number[d])
         tmp = t2;
     if (t3 != NULL && t3->da.number[d] < tmp->da.number[d])
         tmp = t3;
     return tmp;
 }
-template <class T,class T1>
-shared_ptr<Tnode<T,T1>> findMinRec(shared_ptr<Tnode<T,T1>> root, T1 d) //find minimun consider one dimension
+template <class T, class T1>
+shared_ptr<Tnode<T, T1>> findMinRec(shared_ptr<Tnode<T, T1>> root, T1 d) //find minimun consider one dimension
 {
 
     if (root == NULL)
@@ -237,21 +236,21 @@ shared_ptr<Tnode<T,T1>> findMinRec(shared_ptr<Tnode<T,T1>> root, T1 d) //find mi
         return findMinRec(root->left, d);
     }
 
-    return minNode<T,T1>(root, findMinRec(root->left, d), findMinRec(root->right, d), d);
+    return minNode<T, T1>(root, findMinRec(root->left, d), findMinRec(root->right, d), d);
 }
-template <class T,class T1>
-shared_ptr<Tnode<T,T1>> findMin(shared_ptr<Tnode<T,T1>> root, T1 d)
+template <class T, class T1>
+shared_ptr<Tnode<T, T1>> findMin(shared_ptr<Tnode<T, T1>> root, T1 d)
 {
     return findMinRec(root, d);
 } //find minimum in d th dimension
-template <class T,class T1>
+template <class T, class T1>
 void copyPoint(T p1[], T p2[])
 {
     for (int i = 0; i < k; i++)
         p1[i] = p2[i];
 }
-template <class T,class T1>
-shared_ptr<Tnode<T,T1>> deleteNodeRec(shared_ptr<Tnode<T,T1>> root, T point[])
+template <class T, class T1>
+shared_ptr<Tnode<T, T1>> deleteNodeRec(shared_ptr<Tnode<T, T1>> root, T point[])
 {
     // if point is not present
     if (root == NULL)
@@ -261,24 +260,24 @@ shared_ptr<Tnode<T,T1>> deleteNodeRec(shared_ptr<Tnode<T,T1>> root, T point[])
     int cd = root->split;
 
     // if the point  is present at root
-    if (arePointsSame<T,T1>(root->da.number, point))
+    if (arePointsSame<T, T1>(root->da.number, point))
     {
 
         if (root->right != NULL)
         {
             // Find minimum of root's dimension in right subtree
-            shared_ptr<Tnode<T,T1>> min = findMin(root->right, cd);
+            shared_ptr<Tnode<T, T1>> min = findMin(root->right, cd);
 
             // copy the minimum to root
-            copyPoint<T,T1>(root->da.number, min->da.number);
+            copyPoint<T, T1>(root->da.number, min->da.number);
 
             // recursively delete the minimum
             root->right = deleteNodeRec(root->right, min->da.number);
         }
         else if (root->left != NULL) // same as above
         {
-            shared_ptr<Tnode<T,T1>> min = findMin(root->left, cd);
-            copyPoint<T,T1>(root->da.number, min->da.number);
+            shared_ptr<Tnode<T, T1>> min = findMin(root->left, cd);
+            copyPoint<T, T1>(root->da.number, min->da.number);
             root->right = deleteNodeRec(root->left, min->da.number);
         }
         else // if node to be deleted is leaf node
@@ -294,17 +293,17 @@ shared_ptr<Tnode<T,T1>> deleteNodeRec(shared_ptr<Tnode<T,T1>> root, T point[])
         root->right = deleteNodeRec(root->right, point);
     return root;
 }
-template <class T,class T1>
-shared_ptr<Tnode<double,int>> deleteNode(shared_ptr<Tnode<double,int>> root, T point[]) //delete node
+template <class T, class T1>
+shared_ptr<Tnode<double, int>> deleteNode(shared_ptr<Tnode<double, int>> root, T point[]) //delete node
 {
-    return deleteNodeRec<T,T1>(root, point);
+    return deleteNodeRec<T, T1>(root, point);
 }
 template <class T, class T1>
-int deletedata(vectordata<T,T1> set[], T point1[], T1 id)
+int deletedata(vectordata<T, T1> set[], T point1[], T1 id)
 {
     for (int i = 0; i < id; i++)
     {
-        if (arePointsSame<T,T1>(set[i].number, point1)) //find the set and delete the set
+        if (arePointsSame<T, T1>(set[i].number, point1)) //find the set and delete the set
         {
             for (int j = i; j < id - 1; j++)
             {
